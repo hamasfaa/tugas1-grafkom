92b9b837a1de822663f2c8f5f76375d710612273
Hamasah Fatiy Dakhilullah <hamasfa@gmail.com>
2025-10-09 09:55:11 +0700

    feat: add lighting controls and implement lighting calculations in shaders

--- if.js
+++ if.js
@@ -3,6 +3,7 @@ var gl;
 
 var vertices = [];
 var colors = [];
+var normals = [];
 var indices = [];
 var numElements = 0;
 
@@ -31,6 +32,8 @@ var bottom = -4.0;
 var modelViewMatrix, projectionMatrix;
 var modelViewMatrixLoc, projectionMatrixLoc;
 
+var aspect = 800 / 600;
+
 var zoomLevel = 1.0;
 var rotationX = 0.0;
 var rotationY = 0.0;
@@ -41,7 +44,18 @@ var autoRotating = false;
 
 var vBuffer;
 var cBuffer;
+var nBuffer;
 var iBuffer;
+
+var lightPosition = vec3(5.0, 5.0, 10.0);
+var ambientLight = vec3(0.2, 0.2, 0.2);
+var diffuseLight = vec3(0.8, 0.8, 0.8);
+var specularLight = vec3(1.0, 1.0, 1.0);
+var shininess = 32.0;
+var enableLighting = true;
+
+var lightPositionLoc, ambientLightLoc, diffuseLightLoc, specularLightLoc;
+var shininessLoc, enableLightingLoc, normalMatrixLoc;
 
 var brickColors = [
     vec4(60 / 255, 56 / 255, 47 / 255, 1.0),
@@ -90,6 +104,13 @@ window.onload = function init() {
     gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
 
+    nBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, nBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(normals), gl.STATIC_DRAW);
+    var vNormal = gl.getAttribLocation(program, "vNormal");
+    gl.vertexAttribPointer(vNormal, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vNormal);
+
     // 3. Index Buffer (IBO)
     iBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
@@ -97,6 +118,14 @@ window.onload = function init() {
 
     modelViewMatrixLoc = gl.getUniformLocation(program, "uModelViewMatrix");
     projectionMatrixLoc = gl.getUniformLocation(program, "uProjectionMatrix");
+    normalMatrixLoc = gl.getUniformLocation(program, "uNormalMatrix");
+
+    lightPositionLoc = gl.getUniformLocation(program, "uLightPosition");
+    ambientLightLoc = gl.getUniformLocation(program, "uAmbientLight");
+    diffuseLightLoc = gl.getUniformLocation(program, "uDiffuseLight");
+    specularLightLoc = gl.getUniformLocation(program, "uSpecularLight");
+    shininessLoc = gl.getUniformLocation(program, "uShininess");
+    enableLightingLoc = gl.getUniformLocation(program, "uEnableLighting");
 
     initControls();
     render();
@@ -106,6 +135,7 @@ function createScene() {
     vertices = [];
     colors = [];
     indices = [];
+    normals = [];
     numElements = 0;
 
     createBrickWall();
@@ -129,9 +159,15 @@ function quad(a, b, c, d, color) {
     var localVertices = [a, b, c, d];
     var baseIndex = vertices.length;
 
+    // Calculate normal for this quad
+    var v1 = subtract3(b, a);
+    var v2 = subtract3(c, a);
+    var normal = normalize3(cross3(v1, v2));
+
     for (var i = 0; i < localVertices.length; i++) {
         vertices.push(localVertices[i]);
         colors.push(color);
+        normals.push(normal);
     }
 
     indices.push(baseIndex, baseIndex + 1, baseIndex + 2);
@@ -165,24 +201,31 @@ function createCube(transformMatrix, color, backColor) {
     }
 
     var baseIndex = vertices.length;
-    for (var k = 0; k < transformedVertices.length; k++) {
-        vertices.push(transformedVertices[k]);
-        colors.push(k === 4 || k === 5 || k === 6 || k === 7 ? (backColor || color) : color);
-    }
 
-    var localIndices = [
-        1, 0, 3, 1, 3, 2, // Front
-        2, 3, 7, 2, 7, 6, // Right
-        3, 0, 4, 3, 4, 7, // Bottom
-        6, 5, 1, 6, 1, 2, // Top
-        4, 5, 6, 4, 6, 7, // Back
-        5, 4, 0, 5, 0, 1  // Left
+    var faces = [
+        { indices: [1, 0, 3, 2], normal: vec3(0, 0, 1) },   // Front
+        { indices: [2, 3, 7, 6], normal: vec3(1, 0, 0) },   // Right
+        { indices: [3, 0, 4, 7], normal: vec3(0, -1, 0) },  // Bottom
+        { indices: [6, 5, 1, 2], normal: vec3(0, 1, 0) },   // Top
+        { indices: [4, 5, 6, 7], normal: vec3(0, 0, -1) },  // Back
+        { indices: [5, 4, 0, 1], normal: vec3(-1, 0, 0) }   // Left
     ];
 
-    for (var l = 0; l < localIndices.length; l++) {
-        indices.push(baseIndex + localIndices[l]);
+    for (var f = 0; f < faces.length; f++) {
+        var face = faces[f];
+        var faceColor = (f === 4 && backColor) ? backColor : color;
+
+        for (var i = 0; i < 4; i++) {
+            vertices.push(transformedVertices[face.indices[i]]);
+            colors.push(faceColor);
+            normals.push(face.normal);
+        }
+
+        var idx = baseIndex + f * 4;
+        indices.push(idx, idx + 1, idx + 2);
+        indices.push(idx, idx + 2, idx + 3);
+        numElements += 6;
     }
-    numElements += localIndices.length;
 }
 
 
@@ -298,6 +341,32 @@ function initControls() {
     translateXSlider.oninput = function () { translationX = parseFloat(this.value); document.getElementById("translateXValue").innerHTML = translationX.toFixed(1); };
     var translateYSlider = document.getElementById("translateY");
     translateYSlider.oninput = function () { translationY = parseFloat(this.value); document.getElementById("translateYValue").innerHTML = translationY.toFixed(1); };
+
+    // Lighting controls
+    var lightPosXSlider = document.getElementById("lightPosX");
+    lightPosXSlider.oninput = function () { lightPosition[0] = parseFloat(this.value); document.getElementById("lightPosXValue").innerHTML = this.value; };
+    var lightPosYSlider = document.getElementById("lightPosY");
+    lightPosYSlider.oninput = function () { lightPosition[1] = parseFloat(this.value); document.getElementById("lightPosYValue").innerHTML = this.value; };
+    var lightPosZSlider = document.getElementById("lightPosZ");
+    lightPosZSlider.oninput = function () { lightPosition[2] = parseFloat(this.value); document.getElementById("lightPosZValue").innerHTML = this.value; };
+
+    // Ambient
+    document.getElementById("ambientR").oninput = function () { ambientLight[0] = parseFloat(this.value); document.getElementById("ambientRValue").innerHTML = this.value; };
+    document.getElementById("ambientG").oninput = function () { ambientLight[1] = parseFloat(this.value); document.getElementById("ambientGValue").innerHTML = this.value; };
+    document.getElementById("ambientB").oninput = function () { ambientLight[2] = parseFloat(this.value); document.getElementById("ambientBValue").innerHTML = this.value; };
+
+    // Diffuse
+    document.getElementById("diffuseR").oninput = function () { diffuseLight[0] = parseFloat(this.value); document.getElementById("diffuseRValue").innerHTML = this.value; };
+    document.getElementById("diffuseG").oninput = function () { diffuseLight[1] = parseFloat(this.value); document.getElementById("diffuseGValue").innerHTML = this.value; };
+    document.getElementById("diffuseB").oninput = function () { diffuseLight[2] = parseFloat(this.value); document.getElementById("diffuseBValue").innerHTML = this.value; };
+
+    // Specular
+    document.getElementById("specularR").oninput = function () { specularLight[0] = parseFloat(this.value); document.getElementById("specularRValue").innerHTML = this.value; };
+    document.getElementById("specularG").oninput = function () { specularLight[1] = parseFloat(this.value); document.getElementById("specularGValue").innerHTML = this.value; };
+    document.getElementById("specularB").oninput = function () { specularLight[2] = parseFloat(this.value); document.getElementById("specularBValue").innerHTML = this.value; };
+
+    // Shininess
+    document.getElementById("shininess").oninput = function () { shininess = parseFloat(this.value); document.getElementById("shininessValue").innerHTML = this.value; };
 }
 
 function resetView() {
@@ -308,6 +377,38 @@ function resetView() {
     document.getElementById("rotateZ").value = 0; document.getElementById("rotateZValue").innerHTML = "0°";
     document.getElementById("translateX").value = 0; document.getElementById("translateXValue").innerHTML = "0.0";
     document.getElementById("translateY").value = 0; document.getElementById("translateYValue").innerHTML = "0.0";
+}
+
+function resetLighting() {
+    lightPosition = vec3(5.0, 5.0, 10.0);
+    ambientLight = vec3(0.2, 0.2, 0.2);
+    diffuseLight = vec3(0.8, 0.8, 0.8);
+    specularLight = vec3(1.0, 1.0, 1.0);
+    shininess = 32.0;
+    enableLighting = true;
+
+    document.getElementById("lightPosX").value = 5.0; document.getElementById("lightPosXValue").innerHTML = "5.0";
+    document.getElementById("lightPosY").value = 5.0; document.getElementById("lightPosYValue").innerHTML = "5.0";
+    document.getElementById("lightPosZ").value = 10.0; document.getElementById("lightPosZValue").innerHTML = "10.0";
+
+    document.getElementById("ambientR").value = 0.2; document.getElementById("ambientRValue").innerHTML = "0.2";
+    document.getElementById("ambientG").value = 0.2; document.getElementById("ambientGValue").innerHTML = "0.2";
+    document.getElementById("ambientB").value = 0.2; document.getElementById("ambientBValue").innerHTML = "0.2";
+
+    document.getElementById("diffuseR").value = 0.8; document.getElementById("diffuseRValue").innerHTML = "0.8";
+    document.getElementById("diffuseG").value = 0.8; document.getElementById("diffuseGValue").innerHTML = "0.8";
+    document.getElementById("diffuseB").value = 0.8; document.getElementById("diffuseBValue").innerHTML = "0.8";
+
+    document.getElementById("specularR").value = 1.0; document.getElementById("specularRValue").innerHTML = "1.0";
+    document.getElementById("specularG").value = 1.0; document.getElementById("specularGValue").innerHTML = "1.0";
+    document.getElementById("specularB").value = 1.0; document.getElementById("specularBValue").innerHTML = "1.0";
+
+    document.getElementById("shininess").value = 32; document.getElementById("shininessValue").innerHTML = "32";
+    document.getElementById("enableLighting").checked = true;
+}
+
+function toggleLighting() {
+    enableLighting = document.getElementById("enableLighting").checked;
 }
 
 function autoRotate() {
@@ -392,7 +493,18 @@ function render() {
     modelViewMatrix = mult(modelViewMatrix, rotateZ(radians(rotationZ)));
     gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
 
-    // --- Menggambar menggunakan IBO ---
+    // Calculate and set normal matrix
+    var normalMatrix = mat3FromMat4(modelViewMatrix);
+    gl.uniformMatrix3fv(normalMatrixLoc, false, flatten3(normalMatrix));
+
+    // Set lighting uniforms
+    gl.uniform3fv(lightPositionLoc, lightPosition);
+    gl.uniform3fv(ambientLightLoc, ambientLight);
+    gl.uniform3fv(diffuseLightLoc, diffuseLight);
+    gl.uniform3fv(specularLightLoc, specularLight);
+    gl.uniform1f(shininessLoc, shininess);
+    gl.uniform1i(enableLightingLoc, enableLighting);
+
     gl.drawElements(gl.TRIANGLES, numElements, gl.UNSIGNED_SHORT, 0);
 
     requestAnimFrame(render);
@@ -451,6 +563,52 @@ function ortho(left, right, bottom, top, near, far) {
     result[1][3] = -(top + bottom) / h;
     result[2][3] = -(near + far) / d;
     return result;
+}
+
+function vec3(x, y, z) {
+    return [x, y, z];
+}
+
+function subtract3(a, b) {
+    return vec3(a[0] - b[0], a[1] - b[1], a[2] - b[2]);
+}
+
+function cross3(a, b) {
+    return vec3(
+        a[1] * b[2] - a[2] * b[1],
+        a[2] * b[0] - a[0] * b[2],
+        a[0] * b[1] - a[1] * b[0]
+    );
+}
+
+function normalize3(v) {
+    var len = Math.sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+    if (len > 0) {
+        return vec3(v[0] / len, v[1] / len, v[2] / len);
+    }
+    return v;
+}
+
+function mat3FromMat4(m) {
+    var result = [];
+    for (var i = 0; i < 3; i++) {
+        result[i] = [];
+        for (var j = 0; j < 3; j++) {
+            result[i][j] = m[i][j];
+        }
+    }
+    return result;
+}
+
+function flatten3(m) {
+    var floats = new Float32Array(9);
+    var idx = 0;
+    for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+            floats[idx++] = m[j][i];
+        }
+    }
+    return floats;
 }
 
 function mat4() {
